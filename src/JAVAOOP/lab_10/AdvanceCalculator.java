252dfc7d849f72ce87d838bce83c18cdfda180fd
Sattz123 <class35123@gmail.com>
2025-02-20 00:47:37 +0700

    advancecalculator update

--- src/JAVAOOP/lab_10/AdvanceCalculator.java
+++ src/JAVAOOP/lab_10/AdvanceCalculator.java
@@ -4,7 +4,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.Random;
+
 
 public class AdvanceCalculator {
     static double result = 0;
